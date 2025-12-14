e16a311d3358d1361dbb9cb112b4eed84baec370
Kevin-04-cpu <paradacastillok@gmail.com>
2025-12-14 14:41:05 -0500

    Actualización backend: Dockerfile, build.gradle y RecycleApplication

--- recycle/src/main/java/com/sistem/recycle/RecycleApplication.java
+++ recycle/src/main/java/com/sistem/recycle/RecycleApplication.java
@@ -2,8 +2,10 @@ package com.sistem.recycle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.sistem.recycle.Repositories")
 public class RecycleApplication {
 
 	public static void main(String[] args) {
